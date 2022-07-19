9391cd0d277728317f0dd672884a45b16c7dba9f
cnaveiro <cnaveiro@fen.uchile.cl>
2022-07-19 18:03:19 -0400

    pre gerson

--- src/app/@admin/core/component/title/title.component.ts
+++ src/app/@admin/core/component/title/title.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-title',
+  selector: 'app-admin-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
--- src/app/@admin/pages/admin.module.ts
+++ src/app/@admin/pages/admin.module.ts
@@ -5,12 +5,14 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { HeaderComponent } from '../core/component/header/header.component';
 import { TitleComponent } from './../core/component/title/title.component';
+import { SidebarComponent } from '../core/component/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AdminComponent,
     HeaderComponent,
     TitleComponent,
+    SidebarComponent,
   ],
   imports: [
     CommonModule,
